b1407ad38a84f19723809cbafac5a233ecdb0524
gaytrisran03 <gaytrisran03@gmail.com>
2023-08-29 12:54:25 +0530

    lab-2 final

--- lab-2/lab-2.py
+++ lab-2/lab-2.py
@@ -1,15 +1,29 @@
+import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+
+# Print NLTK version
+print("NLTK version:", nltk.__version__)
 
 # Initialize the Porter Stemmer
 porter = PorterStemmer()
 
-text = "The quick brown foxes are jumping over the lazy dogs' tails."
+def custom_stem(word):
+    if word.endswith("ies") and len(word) > 3 and word[-4] not in "aeiou":
+        return word[:-3] + "y"
+    if word.endswith("able") and len(word) > 5 and word[-5] in "aeiou":
+        return word[:-4] + "e"
+    if word.endswith("able") and len(word) > 5:
+        return word[:-4]
+    
+    return porter.stem(word)
+
+text = "tails and playing with babies parties companies studies breakable Readable Portable Enjoyable Adaptable Valuable Observable"
 
 words = word_tokenize(text)
 
 print("Original Word\t\tStemmed Word")
 print("----------------------------------------")
 for word in words:
-    stemmed_word = porter.stem(word)
-    print(f"{word}\t\t\t{stemmed_word}")
+    stemmed_word = custom_stem(word)
+    print(f"{word}\t\t\t\t{stemmed_word}")
