0e268edb044e1bd53a2b08386a842dbd4caa5cfb
gaytrisran03 <gaytrisran03@gmail.com>
2023-11-21 12:37:26 +0530

    final code for lab-10

--- lab-10/lab-10.py
+++ lab-10/lab-10.py
@@ -1,50 +1,53 @@
 import nltk
-from nltk import bigrams
+from nltk import ngrams
 from nltk.corpus import words
+from tabulate import tabulate
 
 nltk.download('words')
 
-
-def build_bigram_set():
+def build_ngram_set(n):
     english_corpus = set(words.words())
-    bigram_set = set()
+    ngram_set = set()
 
     for word in english_corpus:
-        bigram_set.update(set(bigrams(word.lower())))
+        ngram_set.update(set(ngrams(word.lower(), n)))
 
-    return bigram_set
+    return ngram_set
 
+def get_ngrams(word, n):
+    return set(ngrams(word.lower(), n))
 
-def get_bigrams(word):
-    return set(bigrams(word.lower()))
+def calculate_ngram_similarity(input_word, candidate_word, n):
+    input_ngrams = get_ngrams(input_word, n)
+    candidate_ngrams = get_ngrams(candidate_word, n)
+    common_ngrams = input_ngrams.intersection(candidate_ngrams)
+    similarity = len(common_ngrams) / max(len(input_ngrams), len(candidate_ngrams))
+    return similarity, common_ngrams
 
-
-def calculate_bigram_similarity(input_word, candidate_word):
-    input_bigrams = get_bigrams(input_word)
-    candidate_bigrams = get_bigrams(candidate_word)
-    common_bigrams = input_bigrams.intersection(candidate_bigrams)
-    similarity = len(common_bigrams) / max(len(input_bigrams), len(candidate_bigrams))
-    return similarity
-
-
-def spell_check(input_word, bigram_set, word_set):
-    suggestions = sorted(word_set, key=lambda x: calculate_bigram_similarity(input_word, x), reverse=True)
-    top_suggestions = suggestions[:5]
+def spell_check(input_word, ngram_set, word_set, n):
+    suggestions = sorted(word_set, key=lambda x: calculate_ngram_similarity(input_word, x, n), reverse=True)
+    top_suggestions = suggestions[:3]
     return top_suggestions
 
-
 if __name__ == "__main__":
-    # Build bigram set and word set
-    bigram_set = build_bigram_set()
+    # Set the size of the n-grams (bigrams and trigrams in this case)
+    bigram_set = build_ngram_set(2)
+    trigram_set = build_ngram_set(3)
     word_set = set(words.words())
 
     # Input misspelled word
     misspelled_word = input("Enter a misspelled word: ")
 
-    # Spell check using bigram similarity
-    suggestions = spell_check(misspelled_word, bigram_set, word_set)
+    # Spell check using both bigrams and trigrams
+    suggestions = spell_check(misspelled_word, trigram_set, word_set, 3)
 
-    # Display top 5 suggestions
-    print("Top 5 suggestions:")
+    # Create a table with suggested words and the lengths of common bigrams and trigrams
+    table_data = []
     for suggestion in suggestions:
-        print(suggestion)
+        similarity, common_bigrams = calculate_ngram_similarity(misspelled_word, suggestion, 2)
+        _, common_trigrams = calculate_ngram_similarity(misspelled_word, suggestion, 3)
+        table_data.append([suggestion, len(common_bigrams), len(common_trigrams)])
+
+    # Display the table
+    headers = ["Word", "Length of Common Bigrams", "Length of Common Trigrams"]
+    print(tabulate(table_data, headers=headers, tablefmt="grid"))
